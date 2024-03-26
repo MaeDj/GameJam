d327cf3e301f0579a80b2ce68d8be5a7df5d3593
Dugoua-Jacques Maé <152476556+MaeDj@users.noreply.github.com>
2024-03-26 13:58:25 +0100

    update evenement

--- GameJamJeuOie/src/gamejamjeuoie/Bonus.java
+++ GameJamJeuOie/src/gamejamjeuoie/Bonus.java
@@ -10,20 +10,20 @@ public class Bonus extends Evenement {
     int points;
 
     public Bonus(String nom) {
-        super(nom);
+        super();
         Random rand = new Random();
         int choix = rand.nextInt(4);
         if (choix == 0) {
-             = "Tres bien, vous maitrisez votre cours ! ";
+             super.nom= "Tres bien, vous maitrisez votre cours ! ";
             points = 2;
         } else if (choix == 1) {
-            phraseAleatoire = "Super, vous etes toujours present en cours ! ";
+            super.nom = "Super, vous etes toujours present en cours ! ";
             points = 1;
         } else if(choix==2){
-            phraseAleatoire = "Excellent, vous avez reussi votre semestre ! ";
+            super.nom = "Excellent, vous avez reussi votre semestre ! ";
             points = 5;
         }else{
-            phraseAleatoire = "Bravo, vous avez participe à la GameJam ! ";
+            super.nom = "Bravo, vous avez participe à la GameJam ! ";
             points = 3;
         }
     }
