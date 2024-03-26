c6bc65531455bd51d5faaba83e54c653d4ae257d
Roinuj22 <165004628+Roinuj22@users.noreply.github.com>
2024-03-26 13:36:52 +0100

    test

---
+++ GameJamJeuOie/src/gamejamjeuoie/Evenement.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gamejamjeuoie;
+
+/**
+ *
+ * @author Junior
+ */
+public class Evenement {
+        String nom;
+
+    public Evenement(String nom) {
+        this.nom = nom;
+    }
+
+    public abstract String toString();
+}
