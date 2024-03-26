bd74810efcae846accf71e102e0c875e043a7e7d
Dugoua-Jacques Maé <152476556+MaeDj@users.noreply.github.com>
2024-03-26 10:20:00 +0100

    test

--- GameJamJeuOie/src/gamejamjeuoie/GameJamJeuOie.java
+++ GameJamJeuOie/src/gamejamjeuoie/GameJamJeuOie.java
@@ -9,12 +9,13 @@ package gamejamjeuoie;
  * @author mae
  */
 public class GameJamJeuOie {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+            System.out.println("a");
     }
     
 }
