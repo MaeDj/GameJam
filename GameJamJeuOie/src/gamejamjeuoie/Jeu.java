087a926204f4cfd57d70e33e6a3907b3e44d2afa
KomaSans8 <romainthuriot@gmail.com>
2024-03-26 15:40:56 +0100

    jeu

--- GameJamJeuOie/src/gamejamjeuoie/Jeu.java
+++ GameJamJeuOie/src/gamejamjeuoie/Jeu.java
@@ -15,31 +15,39 @@ public class Jeu implements Listevent {
 
     Plateau plateau1; // le plateau sur lequel on joue 
     Joueur j1;// le joueur qui joue 
-    int de; // décide au hasard de combiens de cases va être avancé le Joueur 
+    int de,i; // décide au hasard de combiens de cases va être avancé le Joueur 
 
     public void jeu() {
-        boolean recommencer;
+        boolean recommencer=true;
+        while(recommencer){
         System.out.println("----début de la partie----");
-        for (int i = 0; i < plateau1.plateau.size(); i++) {
+        j1=new Joueur();
+        plateau1=new Plateau();
+        i=0;
+        while(i<plateau1.plateau.size()) {
             System.out.println("Voici votre plateau ");
             System.out.println(plateau1); //creer la toString du plateau 
             Random ra = new Random();
             de = ra.nextInt();
             System.out.println("D'après le dé, Vous allez avancer de " + de);
             j1.avancer(de);//permet d'avancer le joueur du nombre de cases de ton dé
+            i+=de; 
             System.out.println(plateau1.get(j1.getPosition()));
             if (plateau1.get(j1.getPosition()) instanceof Malus) {//si l'event est un malus
 
                 Malus malus = (Malus) plateau1.get(j1.getPosition());
                 System.out.println(malus);
-                j1.setNbPt(j1.getNbPt() - (plateau1.get(j1.getPosition()).pt));
+                j1.setNbPt(j1.getNbPt() - (malus.points));
             } else if (plateau1.get(j1.getPosition()) instanceof Bonus) {// si l'event est un bonus 
 
                 Bonus bonus = (Bonus) plateau1.get(j1.getPosition());
                 System.out.println(bonus);
-                j1.setNbPt(j1.getNbPt() + (plateau1.get(j1.getPosition()).pt));// on ajoute au joueur son nb de points 
+                j1.setNbPt(j1.getNbPt() + (bonus.points));// on ajoute au joueur son nb de points 
             } else if (plateau1.get(j1.getPosition()) instanceof Question) {// si l'event est une question
                 Question question = (Question) plateau1.get(j1.getPosition());
+                while(!plateau1.verifQuestion(question.nom)){
+                    question = (Question) plateau1.get(j1.getPosition());
+                }    
                 int nbPtGagneOuPerdu = question.poserQuestion();//on récupère avec la méthode posé question le nb de points gagnés ou perdus pour le joueur 
                 if (j1.isaUnBonus() && nbPtGagneOuPerdu > 0) {
                     System.out.println("Grace à votre précédente action vous remportez un point de plus à la question ");
@@ -58,7 +66,7 @@ public class Jeu implements Listevent {
             }
         }
         if (j1.getNbPt() >= 10) {
-            System.out.println("Vous avez remporté la partie! Le précieux diplôme est entre vos main!");
+            System.out.println("Bravo "+j1.getNom()+" ! Vous avez remporté la partie! Le précieux diplôme est entre vos main!");
         } else {
             System.out.println("Vous n'avez pas 10 de moyenne, vous redoublez ");
             boolean fausserep = true;
@@ -84,6 +92,6 @@ public class Jeu implements Listevent {
                 }
             }
         }
-
+        }
     }
 }
